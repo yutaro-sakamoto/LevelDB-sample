625102f2a9a6d277f08188dc2fec847567f17fbc
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-02-27 19:28:13 +0900

    [Add]: add a sample read/write program

--- app/src/main/java/leveldb/sample/App.java
+++ app/src/main/java/leveldb/sample/App.java
@@ -3,12 +3,26 @@
  */
 package leveldb.sample;
 
+import org.iq80.leveldb.*;
+import static org.iq80.leveldb.impl.Iq80DBFactory.*;
+import java.io.*;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        Options options = new Options();
+        options.createIfMissing(true);
+        try (DB db = factory.open(new File("example"), options);) {
+            db.put(bytes("Tampa"), bytes("rocks"));
+            String value = asString(db.get(bytes("Tampa")));
+            db.delete(bytes("Tampa"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("hello");
     }
 }
